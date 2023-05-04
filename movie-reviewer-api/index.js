4bf2aa644d15d779ccc2588172d66b67c4ae2908
Vicra <honvramirez@gmail.com>
2023-05-03 18:37:05 -0600

    validator and post login

--- movie-reviewer-api/index.js
+++ movie-reviewer-api/index.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const { isEmail, isPassword } = require("./utils/validator");
+app.use(bodyParser.json());
+
+app.post("/login/", function (req, res) {
+  // 0. TODO: middleware
+
+  // 1. verificacion de los parametros (formato)
+  if (isEmail(req.body.email)) {
+    console.log("valido");
+  } else {
+    console.log("no es valido");
+  }
+
+  if (isPassword(req.body.password)) {
+    console.log("password valido");
+  } else {
+    console.log("password no es valido");
+  }
+
+  // 2. TODO: ejecucion del procedimiento
+  // 2.1 validacion en base de datos
+
+  // 3. TODO: mandar una respuesta para cada escenario
+
+  // 4. TODO: control de excepciones try catch
+  res.send({
+    success: true,
+  });
 });
 
 app.listen(3000);
