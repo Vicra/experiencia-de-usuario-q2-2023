d1a21b39e9bda1ab97a9da7d9243cc8085cdb68a
Victor Ramirez <honvramirez@gmail.com>
2023-06-02 18:28:46 -0600

    swagger docs

--- movie-reviewer-api/index.js
+++ movie-reviewer-api/index.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs");
+const YAML = require('yaml');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const cardRouter = require("./routes/card.routes");
 const userRouter = require("./routes/user.routes");
 
 // utils
 app.use(cors());
-
-
 app.use("/cards", cardRouter);
 app.use("/user", userRouter);
-
-
 app.listen(3000);
